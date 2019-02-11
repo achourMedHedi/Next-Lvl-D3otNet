fcc783684b386de9c37de4c3b078cf5058bb9eaa
Achour Mohamed Hedi <40775288+achourMedHedi@users.noreply.github.com>
2019-02-11 02:34:44 +0100

    Add files via upload

---
+++ Part4 (only JWT)/Rest Api/Controllers/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_Api.Controllers
+{
+    public class AppSettings
+{
+    public string Secret { get; set; }
+
+}
+}
